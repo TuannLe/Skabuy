e40bd9db98b0058c6c092fd59bd3a19f10631d8b
Tuấn Lê <tuanle37372000@gmail.com>
2023-03-24 21:06:24 +0700

    upload

--- src/presentation/screen/FavoriteScreen.tsx
+++ src/presentation/screen/FavoriteScreen.tsx
@@ -1,12 +1,21 @@
 import { View, Text, TextInput } from 'react-native'
 import React, { useState } from 'react'
 import tw from 'twrnc'
-import 'react-international-phone/style.css';
+import PhoneInput from 'react-native-phone-input'
 
 export default function FavoriteScreen() {
-    const [phone, setPhone] = useState('');
+    const [value, setValue] = useState()
     return (
-        <View>
+        <View style={tw`w-full h-full px-3`}>
+            <PhoneInput
+                ref={(ref) => { this.phone = ref; }}
+                onPressFlag={this.onPressFlag}
+                initialCountry={'us'}
+                initialValue="13178675309"
+                textProps={{
+                    placeholder: 'Enter a phone number...'
+                }}
+            />
         </View>
     )
 }
